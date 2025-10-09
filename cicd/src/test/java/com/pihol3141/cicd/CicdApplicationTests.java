c5ebbcb60d885cf8a365ff3deb7593bce8c7d959
Tomi <szalaytamas90@gmail.com>
2025-10-09 12:32:08 +0200

    init

---
+++ cicd/src/test/java/com/pihol3141/cicd/CicdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pihol3141.cicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
